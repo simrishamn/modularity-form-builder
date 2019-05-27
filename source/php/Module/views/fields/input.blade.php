51cf38f4c54a852474d7ee64d78d7a8eff6f4159
Patrik Björn <patrik.bjorn@sigma.se>
2019-05-27 10:25:10 +0200

    SIEW-321 Fixed inputs not being required

--- source/php/Module/views/fields/input.blade.php
+++ source/php/Module/views/fields/input.blade.php
@@ -7,19 +7,6 @@ use ModularityFormBuilder\Helper\SanitizeData;
 <div class="grid mod-form-field" {!! $field['conditional_hidden'] !!}>
     <div class="grid-md-12">
         <div class="form-group">
-            <label for="{{ $module_id }}-input-{{ sanitize_title($field['label']) }}">
-                {{ $field['label'] }}
-                @if ($field['required'])
-                    <span class="text-danger">*</span>
-                @endif
-            </label>
-
-            @if (!empty($field['description']))
-                <div class="text-sm text-dark-gray">
-                    {!! SanitizeData::convertLinks($field['description']) !!}
-                </div>
-            @endif
-
             {!! (new ModularityFormBuilder\Helper\RenderInput($field['value_type'], $field['label'], $field)) !!}
 
             @if (isset($field['custom_post_type_title']) && $field['custom_post_type_title'] == true)
